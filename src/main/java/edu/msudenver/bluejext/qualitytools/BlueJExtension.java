76978bbf9a7dfc4f6fdea5fbb4ccfb447682f23b
JuliusRoot31 <55676932+JuliusRoot31@users.noreply.github.com>
2019-11-20 15:00:59 -0700

    updating

---
+++ src/main/java/edu/msudenver/bluejext/qualitytools/BlueJExtension.java
@@ -0,0 +1,5 @@
+package edu.msudenver.bluejext.qualitytools;
+
+public class BlueJExtension {
+
+}
---
+++ src/test/java/edu/msudenver/bluejext/qualitytools/BlueJExtensionTest.java
@@ -0,0 +1,17 @@
+package edu.msudenver.bluejext.qualitytools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class BlueJExtensionTest {
+
+	@Test
+	void test() {
+			
+		BlueJExtension newTest = new BlueJExtension();
+		
+	}
+
+}
